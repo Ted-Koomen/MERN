2f7753dd24eff59cc0750a1a2b23795080dcb56a
Ted-Koomen <koomen.ted@gmail.com>
2017-08-01 12:22:52 -0400

    fix type in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-
+var Comment = require('./model/comments');
 var app = express();
 var router = express.Router();
 
