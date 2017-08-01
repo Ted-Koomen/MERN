a60a24a000ff24841c2976848bef67a680e55eb6
Ted-Koomen <koomen.ted@gmail.com>
2017-08-01 12:37:02 -0400

    connect mongoose to mongo

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict'
+
 
 var express = require('express');
 var mongoose = require('mongoose');
@@ -10,7 +10,10 @@ var router = express.Router();
 var port = process.env.API_PORT || 3001;
 
 //db config
-var mongoDB = 'mongodb://teddykoomen:Koomen21@ds127983.mlab.com:27983/commentstutorial'
+var mongoDB = 'mongodb://teddykoomen:koomen21@ds127983.mlab.com:27983/commentstutorial'
+mongoose.connect(mongoDB, { useMongoClient: true })
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(bodyParser.urlencoded( {extended: true}))
 app.use(bodyParser.json());
@@ -28,9 +31,36 @@ app.use(function(req, res, next) {
   next();
 })
 
-router.get('/', function(req,res) {
-  res.json({message: 'API Initialized!'})
+router.get('/', function(req, res) {
+  res.json({ message: 'API Initialized!'});
 });
+
+//adding the /comments route to our /api router
+router.route('/comments')
+  //retrieve all comments from the database
+  .get(function(req, res) {
+    //looks at our Comment Schema
+    Comment.find(function(err, comments) {
+      if (err)
+        res.send(err);
+      //responds with a json object of our database comments.
+      res.json(comments)
+    });
+  })
+  //post new comment to the database
+  .post(function(req, res) {
+    var comment = new Comment();
+    //body parser lets us use the req.body
+    comment.author = req.body.author;
+    comment.text = req.body.text;
+
+    comment.save(function(err) {
+      if (err)
+        res.send(err);
+      res.json({ message: 'Comment successfully added!' });
+    });
+  });
+
 
 app.use('/api', router);
 
